a89f88744cf46e652e7c862c5e8708659c7d6b2d
bungydotgit <heyimadwaitrao@gmail.com>
2025-09-17 15:47:29 +0530

    test

--- server/src/index.js
+++ server/src/index.js
@@ -16,6 +16,7 @@ const corsOptions = {
   origin: ["http://localhost:5173"],
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "100mb" }));
 app.use(urlencoded({ limit: "100mb", extended: true }));
